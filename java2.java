b95e921a7d6b0ed40c12c1b9fefd28a3209749fa
Anish_Mulla23 <annyaa@gmail.com>
2025-03-28 11:46:39 +0530

    save

---
+++ java2.java
@@ -0,0 +1,11 @@
+package java;
+
+public class myjava{
+    void print(){
+     System.out.println("Hello");
+     System.out.println("Hi");
+    }
+public static void main(String[]args){
+    myjava obj=new myjava();
+    obj.print();
+}}
